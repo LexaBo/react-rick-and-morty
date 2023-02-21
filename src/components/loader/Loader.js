ae198e34cf81fbc96fbaae49c7c5019bf57875e3
Boykov <aboykov@oggettoweb.com>
2023-02-21 12:29:51 +0300

    Fix loader

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -6,8 +6,6 @@ function Loader() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="200"
-      height="200"
       display="block"
       preserveAspectRatio="xMidYMid"
       viewBox="0 0 100 100"
